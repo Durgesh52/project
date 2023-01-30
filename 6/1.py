41c875ebd424854aa32bb7c2e64187b2de84c680
Durgesh52 <89139305+Durgesh52@users.noreply.github.com>
2023-01-30 17:01:21 +0530

    Add files via upload

---
+++ 6/1.py
@@ -0,0 +1,14 @@
+a = 33 
+b = 33
+if b > a :
+    print("b is greater than a")
+else : 
+    print("a and b are equal")
+
+########################################################
+def my_function(fname):
+    print(fname + "Refsnes")
+
+    my_function("Emil")
+    my_function("Tobias")
+    my_function("Linux")
